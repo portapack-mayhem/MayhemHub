89f2fdc1ecd5942114787dbb7dc341f7c2ee5741
jLynx <admin@jlynx.net>
2024-01-01 13:59:00 +1300

    Added connected text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <div>
           <SerialLoader>
-            <p>Hello world!</p>
+            <p>Connected to HackRF!</p>
           </SerialLoader>
         </div>
       </main>
