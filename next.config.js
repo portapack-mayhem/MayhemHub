ab130d728fa70db504530798781d163e7100f394
jLynx <admin@jlynx.net>
2024-01-01 13:51:24 +1300

    Updated export

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 // const withOffline = require('next-offline')
 
-const nextConfig = {}
+const nextConfig = {
+    output: 'export',
+}
 
 // module.exports = withOffline(nextConfig)
 module.exports = nextConfig
