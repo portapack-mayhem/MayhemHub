6665679a9c58b55a33fb0838f498dadb11519222
jLynx <admin@jlynx.net>
2024-01-01 20:37:17 +1300

    Updated to new PWA package

--- next.config.js
+++ next.config.js
@@ -11,11 +11,18 @@ const nextConfig = {
 };
 
 // Configuration object tells the next-pwa plugin
-const withPWA = require("next-pwa")({
+const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public", // Destination directory for the PWA files
   disable: process.env.NODE_ENV === "development", // Disable PWA in development mode
   register: true, // Register the PWA service worker
   skipWaiting: true, // Skip waiting for service worker activation
+  fallbacks: {
+    image: "/static/images/fallback.png",
+    // document: '/other-offline',  // if you want to fallback to a custom    page other than /_offline
+    // font: '/static/font/fallback.woff2',
+    // audio: ...,
+    // video: ...,
+  },
 });
 
 module.exports = withPWA(nextConfig);
---
+++ src/app/offline/page.tsx
@@ -0,0 +1,8 @@
+"use client"
+
+const Fallback = () => {
+  return (
+    <p>Hello, we should be offline</p>
+  )
+}
+export default Fallback
