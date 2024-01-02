e658219735e3cdb86bf35894227338801d188ddc
jLynx <admin@jlynx.net>
2024-01-02 15:23:58 +1300

    Clean up

--- src/app/components/SerialLoader/SerialLoader.tsx
+++ src/app/components/SerialLoader/SerialLoader.tsx
@@ -58,37 +58,55 @@ const SerialLoader = ({ children }: PropsWithChildren<SerialLoaderProps>) => {
     }
   };
 
+  const errorMessage = () => (
+    <div className="absolute inset-0 w-full h-full flex flex-col flex-1 min-h-screen items-center justify-center text-black">
+      <div className="flex flex-col w-full max-w-lg p-6 bg-white rounded-xl">
+        <h1 className="text-xl font-medium -mt-1 mb-2">Error</h1>
+        <p className="mb-1">
+          Your browser doesn&apos;t support the{" "}
+          <a
+            className="text-green-900 hocus:underline"
+            href="https://web.dev/serial/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Web Serial API
+          </a>
+          !
+        </p>
+        <p>Please try switching to a supported browser (e.g., Chrome 89).</p>
+      </div>
+    </div>
+  );
+
+  const connectScreen = () => (
+    <div className="absolute inset-0 w-full h-full flex flex-col flex-1 items-center justify-center text-black">
+      <div className="flex flex-col w-full max-w-4xl p-10 bg-white rounded-3xl">
+        <h1 className="text-4xl font-semibold mb-5">Get Started</h1>
+
+        <p className="text-3xl mb-10 leading-snug">
+          Connect your HackRF via USB to get started.
+        </p>
+
+        <button
+          className="text-3xl text-white bg-green-800 p-5 pb-6 rounded-xl transition-all ring-green-800 ring-0 ring-opacity-50 hocus:bg-green-900 focus:(outline-none ring-8) disabled:(text-gray-500 cursor-not-allowed)"
+          ref={pairButtonRef}
+          disabled={
+            serial.portState === "opening" || serial.portState === "closing"
+          }
+          onClick={onPairButtonClick}
+        >
+          {buttonText}
+        </button>
+      </div>
+    </div>
+  );
   // If can't use serial, return error message
   if (!serial.canUseSerial) {
-    return (
-      <div className="absolute inset-0 w-full h-full flex flex-col flex-1 min-h-screen items-center justify-center text-black">
-        <div className="flex flex-col w-full max-w-lg p-6 bg-white rounded-xl">
-          <h1 className="text-xl font-medium -mt-1 mb-2">Error</h1>
-          <p className="mb-1">
-            Your browser doesn&apos;t support the{" "}
-            <a
-              className="text-green-900 hocus:underline"
-              href="https://web.dev/serial/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Web Serial API
-            </a>
-            !
-          </p>
-          <p>Please try switching to a supported browser (e.g., Chrome 89).</p>
-        </div>
-      </div>
-    );
+    return errorMessage();
   }
 
-  // If autoconnect hasn't run its course yet, wait for that...
-  // if (!serial.hasTriedAutoconnect) {
-  //   return null;
-  // }
-
   // If autoconnect fails, then show manual connect button
-
   let buttonText = "";
   if (serial.portState === "closed") {
     buttonText = "Connect device";
@@ -105,26 +123,7 @@ const SerialLoader = ({ children }: PropsWithChildren<SerialLoaderProps>) => {
           <Fragment>{children}</Fragment>
         </SerialContext.Provider>
       ) : (
-        <div className="absolute inset-0 w-full h-full flex flex-col flex-1 items-center justify-center text-black">
-          <div className="flex flex-col w-full max-w-4xl p-10 bg-white rounded-3xl">
-            <h1 className="text-4xl font-semibold mb-5">Get Started</h1>
-
-            <p className="text-3xl mb-10 leading-snug">
-              Connect your HackRF via USB to get started.
-            </p>
-
-            <button
-              className="text-3xl text-white bg-green-800 p-5 pb-6 rounded-xl transition-all ring-green-800 ring-0 ring-opacity-50 hocus:bg-green-900 focus:(outline-none ring-8) disabled:(text-gray-500 cursor-not-allowed)"
-              ref={pairButtonRef}
-              disabled={
-                serial.portState === "opening" || serial.portState === "closing"
-              }
-              onClick={onPairButtonClick}
-            >
-              {buttonText}
-            </button>
-          </div>
-        </div>
+        connectScreen()
       )}
     </SerialContext.Provider>
   );
