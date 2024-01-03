fe56da195e752b3531a07db4a96e567f4cb14449
jLynx <admin@jlynx.net>
2024-01-03 23:01:22 +1300

    Added repo link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,11 +13,18 @@ const Home = () => {
   );
 
   return (
-    <main className="flex h-full min-h-screen flex-col items-center justify-between p-1">
-      <SerialLoader>
-        <Controller />
-      </SerialLoader>
-    </main>
+    <>
+      <main className="flex h-full min-h-screen flex-col items-center justify-between p-1">
+        <SerialLoader>
+          <Controller />
+        </SerialLoader>
+      </main>
+      <footer className="text-center text-blue-500">
+        <a href="https://github.com/portapack-mayhem/MayhemHub" target="_blank">
+          GitHub Repo
+        </a>
+      </footer>
+    </>
   );
 };
 export default Home;
