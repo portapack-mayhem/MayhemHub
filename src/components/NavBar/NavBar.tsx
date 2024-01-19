6182bce8c36dc6c203702b582315569be148e7e7
pedroCX486 <pedrocx486@gmail.com>
2024-01-19 01:14:09 -0300

    Add NavBar

---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,28 @@
+export const NavBar = () => {
+  return (
+    <div className="navbar bg-gray-700">
+      <div className="flex-1">
+        <a className="btn btn-ghost text-xl">Mayhem Hub</a>
+      </div>
+      <div className="flex-none">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <a href="https://wiki.hackrf.app" target="_blank">
+              Wiki
+            </a>
+          </li>
+          <li>
+            <a href="https://repo.hackrf.app" target="_blank">
+              Repo
+            </a>
+          </li>
+          <li>
+            <a href="https://release.hackrf.app" target="_blank">
+              Firmware Releases
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
