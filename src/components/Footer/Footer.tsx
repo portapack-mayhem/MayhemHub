114db2f1fd8e2493ab45028d4b30b78b71695010
pedroCX486 <pedrocx486@gmail.com>
2024-01-20 15:26:01 -0300

    Change extension and remove copypasta mistake

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,5 @@
 import { faGithubAlt } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import("next").NextConfig;
 
 export const Footer = () => {
   return (
