6f7df68f9a7059ab11857201273e1bc8b31493a0
jLynx <admin@jlynx.net>
2024-01-28 10:20:21 +1300

    Added mac warning message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,6 +15,17 @@ const Home = () => {
     }
   );
 
+  const isMac = () => {
+    if (typeof window !== "undefined") {
+      const userAgent = window.navigator.userAgent;
+
+      if (userAgent.includes("Mac")) {
+        return true;
+      }
+    }
+    return false;
+  };
+
   return (
     <>
       <div className="flex min-h-screen flex-col">
@@ -23,6 +34,21 @@ const Home = () => {
         </header>
         <main className="flex min-h-0 grow flex-col items-center justify-between p-1">
           <SerialLoader>
+            {isMac() && (
+              <p>
+                Looks like you are using a Mac. If you come across any issues,
+                please let us know{" "}
+                <a
+                  className="text-blue-400 underline"
+                  href="https://github.com/portapack-mayhem/MayhemHub/issues/43"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  here
+                </a>
+                .
+              </p>
+            )}
             <Controller />
           </SerialLoader>
         </main>
