33835fe0ed70abd3100bc468b9d53db6223adacc
pedroCX486 <pedrocx486@gmail.com>
2024-01-30 18:49:09 -0300

    Improve navbar layout to better fit mobile screens

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,32 +1,35 @@
+import "./NavBar.css";
+
 export const NavBar = () => {
   return (
-    <div className="navbar bg-gray-700">
-      <div className="flex-1">
-        <a className="btn btn-ghost text-xl">Mayhem Hub</a>
+    <div className="nav bg-neutral">
+      <input type="checkbox" id="nav-check" />
+      <div className="nav-header">
+        <div className="nav-title p-3 text-3xl text-neutral-content">
+          Mayhem Hub
+        </div>
       </div>
-      <div className="hidden flex-none sm:block">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <a href="https://discord.hackrf.app" target="_blank">
-              Discord
-            </a>
-          </li>
-          <li>
-            <a href="https://repo.hackrf.app" target="_blank">
-              Repo
-            </a>
-          </li>
-          <li>
-            <a href="https://wiki.hackrf.app" target="_blank">
-              Wiki
-            </a>
-          </li>
-          <li>
-            <a href="https://release.hackrf.app" target="_blank">
-              Firmware Releases
-            </a>
-          </li>
-        </ul>
+      <div className="nav-btn">
+        <label htmlFor="nav-check">
+          <span></span>
+          <span></span>
+          <span></span>
+        </label>
+      </div>
+
+      <div className="nav-links bg-neutral text-neutral-content">
+        <a href="https://discord.hackrf.app" target="_blank" className="p-4">
+          Discord
+        </a>
+        <a href="https://repo.hackrf.app" target="_blank" className="p-4">
+          Repo
+        </a>
+        <a href="https://wiki.hackrf.app" target="_blank" className="p-4">
+          Wiki
+        </a>
+        <a href="https://release.hackrf.app" target="_blank" className="p-4">
+          Firmware Releases
+        </a>
       </div>
     </div>
   );
