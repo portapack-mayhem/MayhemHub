85239199a4e4dd7041c60a28584b7ff5cffa0aa0
pedroCX486 <pedrocx486@gmail.com>
2024-02-04 16:22:52 -0300

    Apply changes from feedback

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { faTriangleExclamation } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import dynamic from "next/dynamic";
 import Controller from "@/components/Controller/Controller";
 import { Footer } from "@/components/Footer/Footer";
@@ -35,23 +37,10 @@ const Home = () => {
         <main className="flex min-h-0 grow flex-col items-center justify-between p-1">
           <SerialLoader>
             {isMac() && (
-              <div role="alert" className="alert alert-warning m-5 w-[70%]">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6 shrink-0 stroke-current"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
-                  />
-                </svg>
+              <div role="alert" className="alert m-5 mb-0 w-[50%]">
+                <FontAwesomeIcon icon={faTriangleExclamation} />
                 <span>
                   <p>
-                    <p className="text-2xl">Whoa there, cowpoke!</p>
                     Looks like you are using a Mac! If you come across any
                     issues, please let us know{" "}
                     <a
